fef74f59865078beb5e9a73d516786a41e50665f
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-03 14:40:58 +0300

    context finished

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,33 +1,28 @@
-import { useEffect, useState } from "react";
-import WorkoutDetails from "../components/WorkoutDetails";
-import WorkoutForm from "../components/WorkoutForm";
+import { useEffect, useState } from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const Home = () => {
-	const [workouts, setWorkouts] = useState(null);
+	const {workouts, dispatch} = useWorkoutsContext()
 
 	useEffect(() => {
 		const fetchWorkouts = async () => {
-			const res = await fetch("/api/workouts");
+			const res = await fetch('/api/workouts');
 			const data = await res.json();
 
 			if (res.ok) {
-				setWorkouts(data);
+				dispatch({type: "SET_WORKOUTS", payload: data});
 			}
 		};
 		fetchWorkouts();
 	}, []);
-	console.log(workouts);
 	return (
 		<div className="home">
 			<div className="workouts">
 				{workouts &&
 					workouts.map((workout) => {
-						return (
-							<WorkoutDetails
-								key={workout._id}
-								workout={workout}
-							/>
-						);
+						return <WorkoutDetails key={workout._id} workout={workout} />;
 					})}
 			</div>
 			<WorkoutForm />
